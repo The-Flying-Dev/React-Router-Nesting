4fc9215a10ddc4902b2915e5717a75be9cc0fe7a
The-Flying-Dev <leandranm@outlook.com>
2022-07-01 23:04:59 -0700

    Dynamic Generated Nested Route inside Movies

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,33 @@
-function Movies() {
+import { Route, Switch, NavLink } from "react-router-dom";
+import { movies } from "../../data/movieData";
+import MovieDetails from "../MovieDetails";
+
+function Movies({ movies: { id, title, description } }) {
+
+  const handleClick = () => {
+    console.log('Thanks');
+  };
+
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map(movie => (
+          <NavLink 
+            to={`/movies/${movie.id}`} 
+            key={movie.id} 
+            exact={true} 
+            onClick={handleClick}
+          >
+            {movie.title}
+          </NavLink>
+        ))}
+      </nav>
+      <Switch>
+        <Route path='/movies/:movieId'>
+          <MovieDetails />
+        </Route>
+      </Switch>
     </div>
   );
 }
